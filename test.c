d333ed3a357012ee887d58bcc3a7f2b12838f925
Feng Wang <wang6922@126.com>
2022-06-20 16:05:19 +0800

    add read handler test

--- test.c
+++ test.c
@@ -1,18 +1,44 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <stdio.h>
 #include "tev.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
-void init_script(void);
+void periodic_print_hello(void* ctx);
+void cancel_print_hello(void* ctx);
+void read_handler(void* ctx);
+void write_some_data(void* ctx);
+void clear_read_handler(void* ctx);
 
 static tev_handle_t tev = NULL;
 
+/* errors are not handled for simplicity */
 int main(int argc, char const *argv[])
 {
     tev = tev_create_ctx();
 
-    /* user code */
-    init_script();
+    /* user init starts */
+    int fds[2] = {-1,-1};
+    pipe2(fds,O_CLOEXEC|O_NONBLOCK);
+
+    tev_set_read_handler(tev,fds[0],read_handler,&(fds[0]));
+    // periodic_print_hello(NULL);
+    // tev_set_timeout(tev,cancel_print_hello,NULL,5000);
+    tev_set_timeout(tev,write_some_data,&(fds[1]),500);
+    tev_set_timeout(tev,clear_read_handler,&(fds[0]),3000);
+    /* user init ends */
 
     tev_main_loop(tev);
+
+    /* user deinit starts */
+    close(fds[0]);
+    close(fds[1]);
+    /* user deinit ends */
+
     tev_free_ctx(tev);
     return 0;
 }
@@ -30,8 +56,23 @@ void cancel_print_hello(void* ctx)
     tev_clear_timeout(tev,test_timer);
 }
 
-void init_script(void)
+void read_handler(void* ctx)
 {
-    periodic_print_hello(NULL);
-    tev_set_timeout(tev,cancel_print_hello,NULL,5000);
+    char buffer[10] = {0};
+    int fd = *(int*)ctx;
+    ssize_t readlen = read(fd,buffer,sizeof(buffer));
+    printf("Read handler: %s\n",buffer);
+}
+
+void write_some_data(void* ctx)
+{
+    const char* data="hello";
+    int fd = *(int*)ctx;
+    write(fd,data,strlen(data)+1);
+    printf("Write data to pipe.\n");
+}
+
+void clear_read_handler(void* ctx)
+{
+    tev_set_read_handler(tev,*(int*)ctx,NULL,NULL);
 }
